fd9f5d1e385170dab8cc4836dd55f8d73bdf9665
Georgy <gstarikov@gmail.com>
2022-04-01 23:42:42 +0300

    PeopleTriplets - done

--- ms-2020-03/people-triplets.go
+++ ms-2020-03/people-triplets.go
@@ -1,7 +1,5 @@
 package ms_2020_03
 
-import "fmt"
-
 func CountOfTriplets(peoples []int, t int) int {
 	var ret int
 	targetAge := t
@@ -19,7 +17,7 @@ func CountOfTriplets(peoples []int, t int) int {
 			continue
 		}
 		if cnt1 >= 3 && age1*3 == targetAge {
-			ret += 1 * (cnt1 - 1) * (cnt1 - 2) //todo: number of combination
+			ret += calcTripletsCombinations(3, cnt1)
 		}
 		for age2 := age1 + 1; age2 < targetAge; age2++ {
 			cnt2 := m[age2]
@@ -34,18 +32,58 @@ func CountOfTriplets(peoples []int, t int) int {
 			if cnt3 != 0 {
 				var inc int
 				if age3 == age2 {
-					inc = cnt1 * (cnt2 - 1)
+					if cnt2 >= 2 {
+						inc = cnt1 * calcTripletsCombinations(2, cnt2) //number of combinations
+					}
 				} else {
 					inc = cnt1 * cnt2 * cnt3
 				}
-				fmt.Printf("peoples[%+v], age1(%d->%d) age2(%d->%d) age3(%d->%d) decision (%d += %d)\n",
-					peoples,
-					age1, cnt1, age2, cnt2, age3, cnt3,
-					ret, inc)
+				//fmt.Printf("peoples[%+v], age1(%d->%d) age2(%d->%d) age3(%d->%d) decision (%d += %d)\n",
+				//	peoples,
+				//	age1, cnt1, age2, cnt2, age3, cnt3,
+				//	ret, inc)
 
 				ret += inc
 			}
 		}
+	}
+	return ret
+}
+
+/*
+	1 -> 0
+	11 -> 0
+	111 -> 1
+	1111 -> 1110 | 1101 | 1011 | 0111 -> 4
+	11111 ->	11100 | 11010 | 11001 |
+				10110 | 10101 | 10011 |
+				01110 | 01101 | 01011 |
+				00111 -> 10
+	n - total
+	k - subset
+	count = (n+k-1)! / (k!*(n-1)!
+*/
+func calcTripletsCombinations(k, n int) int {
+	//cnt1 := n-k
+	//cnt2 :=
+	if n == k {
+		return 1
+	}
+	p, s := factorial(n), (factorial(k) * factorial(n-k))
+	//fmt.Printf("k(%d) n(%d) %d/%d", k, n, p, s)
+	return p / s
+}
+
+//var preCalcOnce = sync.Once{}
+//var PreCalc = map[int]int
+func factorial(k int) int { //todo caching
+	if k == 0 {
+		return 0
+	}
+	//preCalc.Do()
+	ret := 1
+	for i := 2; i <= k; i++ {
+		ret *= i
 	}
 	return ret
 }
--- ms-2020-03/people-triplets_test.go
+++ ms-2020-03/people-triplets_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func TestPeopleTriplets(t *testing.T) {
 	tests := []struct {
 		p []int
 		t int
@@ -29,12 +29,44 @@ func TestAdd(t *testing.T) {
 		{ //3
 			p: []int{1, 2, 2, 2},
 			t: 5,
-			r: 4,
+			r: 3, //was error in task definition. was 4 must be 3
 		},
 	}
 
 	for i, test := range tests {
 		res := CountOfTriplets(test.p, test.t)
 		assert.Equalf(t, test.r, res, "case(%d) test(%+v) res(%d)", i, test, res)
+	}
+}
+
+func TestFactorial(t *testing.T) {
+	tests := []struct {
+		in, res int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 2},
+		{3, 6},
+		{4, 24},
+	}
+
+	for i, test := range tests {
+		res := factorial(test.in)
+		assert.Equalf(t, test.res, res, "case(%d)", i)
+	}
+}
+
+func TestCalcTripletsCombinations(t *testing.T) {
+	tests := []struct {
+		in, res int
+	}{
+		{3, 1},
+		{4, 4},
+		{5, 10},
+	}
+
+	for i, test := range tests {
+		res := calcTripletsCombinations(3, test.in)
+		assert.Equalf(t, test.res, res, "case(%d)", i)
 	}
 }
