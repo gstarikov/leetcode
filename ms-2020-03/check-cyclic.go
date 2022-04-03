dac56951622c98a55193b83bdce76c6f330d5590
Georgy <gstarikov@gmail.com>
2022-04-03 23:28:45 +0300

    check packages cyclic dependencies - done

--- ms-2020-03/check-cyclic.go
+++ ms-2020-03/check-cyclic.go
@@ -11,33 +11,59 @@ func CheckCyc(packs []pack) bool {
 	for i := range packs {
 		_, ok := m[packs[i].id]
 		if ok {
-			continue // todo: something strange...
+			panic("package list has dublicate entry") // todo: something strange...
 		}
 		m[packs[i].id] = &packs[i]
 	}
 
+	checkedMap := map[string]struct{}{}
 	for _, v := range packs {
-		// internal
-		//visitMap := map[string]struct{}{}
-		//deepCheck(visitMap, m, )
-		for _, d := range v.dep {
-			if d == v.id {
-				continue
-			}
-			//find v.dep[i] -> paks
-			deps, ok := m[d]
-			if !ok {
-				//its strange - internal dep
-				continue
-			}
-
-			for _, dd := range deps.dep {
-				if dd == v.id {
-					//its cyclic
-					return true
-				}
-			}
+		if cyclic := helperCheckDepList(
+			map[string]struct{}{},
+			checkedMap,
+			m,
+			[]string{v.id},
+		); cyclic {
+			return true
 		}
 	}
 	return false
+}
+
+func helperCheckDepList(
+	rootMap map[string]struct{},
+	checkedMap map[string]struct{},
+	packAccessMap map[string]*pack,
+	depList []string,
+) bool {
+	for _, v := range depList {
+		//it is good?
+		if _, ok := checkedMap[v]; ok {
+			continue
+		}
+		//it is cyclic
+		if _, ok := rootMap[v]; ok {
+			return true
+		}
+		//do check
+		elem, ok := packAccessMap[v]
+		if !ok {
+			continue // skip unknown dependencies. alternatove - do panic
+		}
+		//clone root map (its very memory expensive, but alternative - work with sorted array who is cpu expensive)
+		newRoot := map[string]struct{}{}
+		for k := range rootMap {
+			newRoot[k] = struct{}{}
+		}
+		newRoot[elem.id] = struct{}{} //mark it as one of the roots
+
+		deps := elem.dep
+		check := helperCheckDepList(newRoot, checkedMap, packAccessMap, deps)
+		if check == false {
+			checkedMap[v] = struct{}{} //mark as non cyclic
+		} else {
+			return true
+		}
+	}
+	return false // non cyclic
 }
--- ms-2020-03/check-cyclic_test.go
+++ ms-2020-03/check-cyclic_test.go
@@ -10,46 +10,49 @@ func TestCheckCyc(t *testing.T) {
 		arr []pack
 		res bool
 	}{
-		{
+		{ //0
 			arr: []pack{
-				{
-					id:  "a",
-					dep: []string{"b"},
-				},
-				{
-					id:  "b",
-					dep: []string{""},
-				},
+				{id: "a", dep: []string{"b"}},
+				{id: "b", dep: []string{""}},
 			},
 			res: false,
 		},
-		{
+		{ //1
 			arr: []pack{
-				{
-					id:  "a",
-					dep: []string{"b"},
-				},
-				{
-					id:  "b",
-					dep: []string{"a"},
-				},
+				{id: "a", dep: []string{"b"}},
+				{id: "b", dep: []string{"a"}},
 			},
 			res: true,
 		},
-		{
+		{ //2
 			arr: []pack{
-				{
-					id:  "a",
-					dep: []string{"b"},
-				},
-				{
-					id:  "b",
-					dep: []string{"c"},
-				},
-				{
-					id:  "c",
-					dep: []string{"a"},
-				},
+				{id: "a", dep: []string{"b"}},
+				{id: "b", dep: []string{"c"}},
+				{id: "c", dep: []string{"a"}},
+			},
+			res: true,
+		},
+		{ //3
+			arr: []pack{
+				{id: "a", dep: []string{"b", "c"}},
+				{id: "b", dep: []string{}},
+				{id: "c", dep: []string{"b"}},
+			},
+			res: false,
+		},
+		{ //4
+			arr: []pack{
+				{id: "a", dep: []string{"b", "c"}},
+				{id: "b", dep: []string{"c"}},
+				{id: "c", dep: []string{}},
+			},
+			res: false,
+		},
+		{ //5
+			arr: []pack{
+				{id: "a", dep: []string{"b", "c"}}, //test for hangup
+				{id: "b", dep: []string{"c"}},
+				{id: "c", dep: []string{"b"}},
 			},
 			res: true,
 		},
@@ -57,7 +60,6 @@ func TestCheckCyc(t *testing.T) {
 
 	for i, test := range tests {
 		res := CheckCyc(test.arr)
-		//t.Errorf()
 		assert.Equalf(t, test.res, res, "case(%d)", i)
 	}
 }
