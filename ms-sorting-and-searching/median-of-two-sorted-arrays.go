6829e7808079b93c17aff13b4adf624362b74b4a
Georgy <gstarikov@gmail.com>
2022-04-17 04:50:30 +0300

    findMedianSortedArraysHint2

--- ms-sorting-and-searching/median-of-two-sorted-arrays.go
+++ ms-sorting-and-searching/median-of-two-sorted-arrays.go
@@ -1,5 +1,7 @@
 package ms_sorting_and_searching
 
+import "math"
+
 /*
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
@@ -27,6 +29,137 @@ Constraints:
 */
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	return helperFindMedianSortedArray(nums1, nums2)
+}
+
+// https://takeuforward.org/data-structure/median-of-two-sorted-arrays-of-different-sizes/
+// но я тут не вижу бинарного поиска :( тут банально начинаем с середины :(
+func findMedianSortedArraysHint2(nums1 []int, nums2 []int) float64 {
+	if len(nums1) > len(nums2) {
+		tmp := nums1
+		nums1 = nums2
+		nums2 = tmp
+	}
+	m, n := len(nums1), len(nums2)
+	low, high, medianPos := 0, m, (m+n+1)/2
+	for low <= high {
+		cut1 := (low + high) >> 1
+		cut2 := medianPos - cut1
+
+		l1 := helperTetrary(cut1 == 0, math.MinInt64, cut1-1, nums1)
+		l2 := helperTetrary(cut2 == 0, math.MinInt64, cut2-1, nums2)
+		r1 := helperTetrary(cut1 == m, math.MaxInt64, cut1, nums1)
+		r2 := helperTetrary(cut2 == n, math.MaxInt64, cut2, nums2)
+
+		if l1 <= r2 && l2 <= r1 {
+			if (m+n)%2 != 0 {
+				return float64(helperMax(l1, l2))
+			}
+			return float64(helperMax(l1, l2)+helperMin(r1, r2)) / 2.0
+		}
+		if l1 > r2 {
+			high = cut1 - 1
+		} else {
+			low = cut1 + 1
+		}
+	}
+	panic("impossible case")
+}
+
+func helperMax(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+func helperMin(a, b int) int {
+	if a >= b {
+		return b
+	}
+	return a
+}
+
+func helperTetrary(cond bool, val, pos int, nums []int) int {
+	if cond {
+		return val
+	}
+	return nums[pos]
+
+}
+
+//https://www.geeksforgeeks.org/median-of-two-sorted-arrays-of-different-sizes/
+// не работает... перепроверил всё, в этом варианте банально кривая разбивка
+func findMedianSortedArraysHint(nums1 []int, nums2 []int) float64 {
+	for {
+		if len(nums1) > len(nums2) {
+			tmp := nums1
+			nums1 = nums2
+			nums2 = tmp
+		}
+
+		switch len(nums1) {
+		case 0:
+			v, _, _ := helperMedianOfOne(nums2)
+			return v
+		case 1:
+			switch {
+			case len(nums2) == 1:
+				return float64(nums1[0]+nums2[0]) / 2.0
+			case len(nums2)%2 == 1:
+				mdIdx := len(nums2) / 2
+				tmp := nums2[mdIdx-1 : mdIdx+2]
+				return helperFindMedianSortedArray(nums1, tmp)
+			case len(nums2)%2 == 0:
+				mdIdx := len(nums2) / 2
+				tmp := nums2[mdIdx-1 : mdIdx+1]
+				return helperFindMedianSortedArray(nums1, tmp)
+			}
+		case 2:
+			switch {
+			case len(nums2) == 2:
+				return helperFindMedianSortedArray(nums1, nums2)
+			case len(nums2)%2 == 1:
+				mdIdx := len(nums2) / 2
+				tmp := nums2[mdIdx-1 : mdIdx+2]
+				return helperFindMedianSortedArray(nums1, tmp)
+			case len(nums2)%2 == 0:
+				mdIdx := len(nums2) / 2
+				tmp := nums2[mdIdx-2 : mdIdx+2]
+				return helperFindMedianSortedArray(nums1, tmp)
+			}
+		}
+
+		// do splitting
+		m1, idx11, idx12 := helperMedianOfOne(nums1)
+		m2, idx21, idx22 := helperMedianOfOne(nums2)
+
+		//decide
+		switch {
+		case m1 < m2:
+			nums1 = nums1[idx11:]
+			nums2 = nums2[:idx22+1]
+		default:
+			nums2 = nums2[idx21:]
+			nums1 = nums1[:idx12+1]
+		}
+	}
+}
+
+func helperMedianOfOne(nums []int) (float64, int, int) {
+	switch ln := len(nums); ln {
+	case 0:
+		return -1, 0, 0
+	default:
+		md1Idx, md2Idx := (ln-1)/2, ln/2
+		if md1Idx == md2Idx {
+			return float64(nums[md1Idx]), md1Idx, md1Idx
+		} else {
+			return float64(nums[md1Idx]+nums[md2Idx]) / 2.0, md1Idx, md2Idx
+		}
+	}
+}
+
+func helperFindMedianSortedArray(nums1 []int, nums2 []int) float64 {
 	total := len(nums1) + len(nums2)
 	if total == 0 {
 		return 0
@@ -66,7 +199,68 @@ func helperPoint(nums1, nums2 []int, p1, p2, pos int, md int) (int, int, int, in
 	return p1, p2, pos, lastVal
 }
 
-func findMedianSortedArraysHint(nums1 []int, nums2 []int) float64 {
-	//https://www.geeksforgeeks.org/median-of-two-sorted-arrays-of-different-sizes/
+func helperMedianOfTwo12(nums1 int, nums2 []int) float64 {
+	if len(nums2) != 2 {
+		panic("invalid input data")
+	}
 
+	// do quick merge and found median
+	switch {
+	case nums1 <= nums2[0]:
+		return float64(nums2[0])
+	case nums1 <= nums2[1]:
+		return float64(nums1)
+	default:
+		return float64(nums2[1])
+	}
+}
+
+func helperMedianOfTwo13(nums1 int, nums2 []int) float64 {
+	if len(nums2) != 3 {
+		panic("invalid input data")
+	}
+
+	// do quick merge and found median
+	switch {
+	case nums1 <= nums2[0]:
+		return float64(nums2[1]+nums2[0]) / 2.0
+	case nums1 <= nums2[2]:
+		return float64(nums1+nums2[1]) / 2.0
+	case nums1 > nums2[2]:
+		return float64(nums2[1]+nums2[2]) / 2.0
+	default:
+		panic("impossible case")
+	}
+}
+
+func helperMedianOfTwo22(nums1, nums2 []int) float64 {
+	if len(nums1) != 2 || len(nums2) != 2 {
+		panic("invalid input data")
+	}
+
+	// do quick merge and found median
+	var m1, m2 float64
+	switch {
+	case nums1[1] <= nums2[0]:
+		return float64(nums1[1]+nums2[0]) / 2.0
+	case nums2[1] <= nums1[0]:
+		return float64(nums1[0]+nums2[1]) / 2.0
+	case nums1[0] >= nums2[0]:
+		m1 = float64(nums1[0])
+		if nums1[1] > nums2[1] {
+			m2 = float64(nums2[1])
+		} else {
+			m2 = float64(nums1[1])
+		}
+	case nums2[0] >= nums1[0]: //DRY !?
+		m1 = float64(nums2[0])
+		if nums2[1] > nums1[1] {
+			m2 = float64(nums1[1])
+		} else {
+			m2 = float64(nums2[1])
+		}
+	default:
+		panic("impossible case")
+	}
+	return (m1 + m2) / 2.0
 }
--- ms-sorting-and-searching/median-of-two-sorted-arrays_test.go
+++ ms-sorting-and-searching/median-of-two-sorted-arrays_test.go
@@ -10,20 +10,95 @@ func TestMedianOfTwoSortedArrays(t *testing.T) {
 		nums1, nums2 []int
 		out          float64
 	}{
-		{
+		{ //0
 			nums1: []int{1, 3},
 			nums2: []int{2},
 			out:   2,
 		},
-		{
+		{ //1
 			nums1: []int{1, 2},
 			nums2: []int{3, 4},
 			out:   2.5,
 		},
+		{ //2
+			nums1: []int{1, 2},
+			nums2: []int{-1, 3},
+			out:   1.5,
+		},
+		{ //3
+			nums1: []int{1},
+			nums2: []int{2, 3, 4},
+			out:   2.5,
+		},
+		{ //4
+			nums1: []int{1, 2, 3},
+			nums2: []int{4, 5, 6, 7},
+			out:   4,
+		},
+		{ //5
+			nums1: []int{2, 2, 4, 4},
+			nums2: []int{2, 2, 4, 4},
+			out:   3,
+		},
+		{ //6
+			nums1: []int{1, 2, 3},
+			nums2: []int{4, 5, 6, 7, 8},
+			out:   4.5,
+		},
+		{ //7
+			nums1: []int{1, 4, 7, 10, 12},
+			nums2: []int{2, 3, 6, 15},
+			out:   6,
+		},
 	}
 
 	for i, test := range tests {
-		res := findMedianSortedArrays(test.nums1, test.nums2)
+		//res := findMedianSortedArrays(test.nums1, test.nums2)
+		//res := findMedianSortedArraysHint(test.nums1, test.nums2)
+		res := findMedianSortedArraysHint2(test.nums1, test.nums2)
+		assert.Equalf(t, test.out, res, "case(%d)", i)
+	}
+}
+
+func TestHelperMedianTwo(t *testing.T) {
+	tests := []struct {
+		nums1, nums2 []int
+		out          float64
+	}{
+		{ //0
+			nums1: []int{1, 2},
+			nums2: []int{3, 4},
+			out:   (2 + 3) / 2.0,
+		},
+		{ //1
+			nums1: []int{3, 4},
+			nums2: []int{1, 2},
+			out:   (2 + 3) / 2.0,
+		},
+		{ //2
+			nums1: []int{1, 4},
+			nums2: []int{2, 3},
+			out:   (2 + 3) / 2.0,
+		},
+		{ //3
+			nums1: []int{2, 3},
+			nums2: []int{1, 4},
+			out:   (2 + 3) / 2.0,
+		},
+		{ //4
+			nums1: []int{1, 3},
+			nums2: []int{2, 4},
+			out:   (2 + 3) / 2.0,
+		},
+		{ //5
+			nums1: []int{2, 4},
+			nums2: []int{1, 3},
+			out:   (2 + 3) / 2.0,
+		},
+	}
+
+	for i, test := range tests {
+		res := helperMedianOfTwo22(test.nums1, test.nums2)
 		assert.Equalf(t, test.out, res, "case(%d)", i)
 	}
 }
