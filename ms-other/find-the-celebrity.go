48f9180dca7835460b97e5f4ec94257f0eea77b1
Georgy <gstarikov@gmail.com>
2022-05-01 22:47:42 +0300

    celebrity

---
+++ ms-other/find-the-celebrity.go
@@ -0,0 +1,107 @@
+package ms_other
+
+/*
+Suppose you are at a party with n people labeled from 0 to n - 1 and among them, there may exist one celebrity.
+The definition of a celebrity is that all the other n - 1 people know the celebrity, but the celebrity does not know any of them.
+
+Now you want to find out who the celebrity is or verify that there is not one.
+The only thing you are allowed to do is ask questions like: "Hi, A. Do you know B?" to get information about whether A knows B.
+You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+
+You are given a helper function bool knows(a, b) that tells you whether A knows B. Implement a function int findCelebrity(n).
+There will be exactly one celebrity if they are at the party.
+
+Return the celebrity's label if there is a celebrity at the party.
+If there is no celebrity, return -1.
+
+Example 1:
+
+Input: graph = [[1,1,0],[0,1,0],[1,1,1]]
+Output: 1
+Explanation: There are three persons labeled with 0, 1 and 2. graph[i][j] = 1 means person i knows person j,
+otherwise graph[i][j] = 0 means person i does not know person j.
+The celebrity is the person labeled as 1 because both 0 and 2 know him but 1 does not know anybody.
+
+Example 2:
+
+Input: graph = [[1,0,1],[1,1,0],[0,1,1]]
+Output: -1
+Explanation: There is no celebrity.
+
+Constraints:
+    n == graph.length
+    n == graph[i].length
+    2 <= n <= 100
+    graph[i][j] is 0 or 1.
+    graph[i][i] == 1
+
+Follow up: If the maximum number of allowed calls to the API knows is 3 * n,
+could you find a solution without exceeding the maximum number of calls?
+*/
+
+/*
+//thing: celebrity has all zeroes in line and all 1 in col
+//
+*/
+
+func solutionMy(knows func(a int, b int) bool) func(n int) int {
+	return func(n int) int {
+		//search for "1" col
+		var i, j int
+		var lastJ int
+	L:
+		for {
+			switch {
+			case i >= n: //out of matrix line, soo there is no celebrity
+				return -1
+			case j >= n: //out of matrix col, soo may be celebrity
+				//do additional check for col - must be all 1
+				for k := 0; k < lastJ; k++ {
+					if !knows(k, i) { //it isnt celebrity
+						i++
+						lastJ = k
+						j = k
+						continue L
+					}
+				}
+				//check for line - all must be 0
+				for k := 0; k < n; k++ {
+					if k == i {
+						continue
+					}
+					if knows(i, k) {
+						return -1
+					}
+				}
+				return i
+			}
+			if knows(j, i) {
+				j++
+				//lastI = i
+			} else {
+				i++
+				lastJ = j
+			}
+		}
+	}
+}
+
+func solutionHint(knows func(a int, b int) bool) func(n int) int {
+	return func(n int) int {
+		var candidate int
+		for i := 1; i < n; i++ {
+			if knows(candidate, i) {
+				candidate = i
+			}
+		}
+		for j := 0; j < n; j++ {
+			if j == candidate {
+				continue
+			}
+			if knows(candidate, j) || !knows(j, candidate) {
+				return -1
+			}
+		}
+		return candidate
+	}
+}
---
+++ ms-other/find-the-celebrity_test.go
@@ -0,0 +1,45 @@
+package ms_other
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	tests := []struct {
+		graph [][]int
+		out   int
+	}{
+		{
+			graph: [][]int{
+				{1, 1, 0},
+				{0, 1, 0},
+				{1, 1, 1},
+			},
+			out: 1,
+		},
+		{
+			graph: [][]int{
+				{1, 0, 1},
+				{1, 1, 0},
+				{0, 1, 1},
+			},
+			out: -1,
+		},
+		{
+			graph: [][]int{
+				{1, 1},
+				{1, 1},
+			},
+			out: -1,
+		},
+	}
+
+	for i, test := range tests {
+		f := solution(func(a int, b int) bool {
+			return test.graph[a][b] == 1
+		})
+		out := f(len(test.graph))
+		assert.Equalf(t, test.out, out, "case(%d)", i)
+	}
+}
