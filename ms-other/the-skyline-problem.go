eedc9a536d08cd553b2e731f9f4a00694054020f
Georgy <gstarikov@gmail.com>
2022-05-11 00:54:09 +0300

    skyline problem

--- ms-other/the-skyline-problem.go
+++ ms-other/the-skyline-problem.go
@@ -1,6 +1,8 @@
 package ms_other
 
-import "sort"
+import (
+	"sort"
+)
 
 /*
 A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance.
@@ -32,6 +34,168 @@ Constraints:
 
 */
 
+//переделать на потоковую версию, работающую с командами "дом появился, дом закончился"
+
+func getSkylineTry3(buildings [][]int) [][]int {
+	var pq pQueue
+	var ret [][]int
+	var i int //building index
+	for {
+		//get building & first end of building
+		var l int
+		okBuilding := i < len(buildings)
+		if okBuilding {
+			building := buildings[i]
+			l = building[0]
+		}
+		pnt, okShadow := pq.Get()
+		if !okShadow && !okBuilding { //done - no more buildings or shadows
+			break
+		}
+		//check who is nearest - new building or end of building
+		switch {
+		case !okShadow && okBuilding: //there is no one building end, so add end of building and start point
+			i = proceedSameStart(buildings, i, 0, &pq, &ret, false)
+		case okShadow && !okBuilding: //no new buildings
+			pq.Remove()
+			pntNxt, ok := pq.Get()
+			vl := []int{pnt.x, 0}
+			if ok {
+				vl[1] = pntNxt.h
+			} //last point H will be zero
+			ret = append(ret, vl)
+			//there is both start and shadow of building
+		case l == pnt.x: //building starts on place of end of other building
+			pq.Remove()
+			i = proceedSameStart(buildings, i, pnt.h, &pq, &ret, true)
+
+		case l < pnt.x: //there is some building
+			i = proceedSameStart(buildings, i, pnt.h, &pq, &ret, false)
+		case l > pnt.x: //shadow ends before new building begins
+			//dont process new building
+			pq.Remove()
+			pntNxt, ok := pq.Get()
+			if ok {
+				ret = append(ret, []int{pnt.x, pntNxt.h})
+			} else {
+				ret = append(ret, []int{pnt.x, 0})
+			}
+		}
+	}
+	return ret
+}
+
+func proceedSameStart(buildings [][]int, pos int, curH int, pq *pQueue, r *[][]int, diffH bool) int {
+	l := buildings[pos][0]
+	pq.Add(point{
+		x: buildings[pos][1],
+		h: buildings[pos][2],
+	})
+	maxH := buildings[pos][2]
+
+	for pos++; pos < len(buildings) && buildings[pos][0] == l; pos++ {
+		x := buildings[pos][1]
+		h := buildings[pos][2]
+		pq.Add(point{
+			x: x,
+			h: h,
+		})
+		if maxH < h {
+			maxH = h
+		}
+	}
+
+	switch {
+	case !diffH && curH < maxH:
+		*r = append(*r, []int{l, maxH})
+	case diffH:
+		pntNxt, _ := pq.Get()
+		if curH != pntNxt.h { //могут так сложиться звёзды, что новая точка на той же высоте как закончившаяся
+			*r = append(*r, []int{l, pntNxt.h})
+		}
+	}
+
+	return pos
+}
+
+type point struct {
+	x, h int
+}
+
+//задачи очереди:
+// хранить пары Х и высота
+// обеспечить учёт множественных пар (добавить и удалить)
+// получение следующей Х точки
+// получение текущей высоты
+type pQueue struct {
+	//optimal structure will be RB\AVL tree
+	//but ill will using ordered slice
+	queu []point
+}
+
+//type pQueueVal struct {
+//	point
+//}
+
+/*
+варианты:
+- Х совпадает. устанавшиваем Н как у самого высокого.
+- Х не совпадает. добавляем.
+пост обработка:
+- проверяем всех предыдущих и устанавливаем Н как у самого высокого
+- убираем дубли по высоте
+*/
+func (p *pQueue) Add(in point) {
+	pos := sort.Search(len(p.queu), func(i int) bool {
+		return p.queu[i].x >= in.x
+	})
+
+	//1 step
+	switch {
+	case pos < len(p.queu) && p.queu[pos].x == in.x:
+		if p.queu[pos].h < in.h {
+			p.queu[pos].h = in.h
+		}
+	case pos < len(p.queu) && p.queu[pos].h >= in.h: //do nothing
+		return
+	default:
+		p.queu = append(p.queu, point{})
+		copy(p.queu[pos+1:], p.queu[pos:])
+		p.queu[pos] = in
+	}
+	//check prev & update prev
+	//binary search !!! because p.queu[:pos-1] must be in decreasing H order
+	//lPos := pos
+	//я ищу первый элемент который меньше либо равен моему.
+	//слева все больше, с точки и далее равны либо меньше
+	lPos := sort.Search(pos, func(i int) bool {
+		return p.queu[i].h <= in.h
+	})
+	//remove shaded buildings
+	switch {
+	case lPos != pos: //found something
+		copy(p.queu[lPos:], p.queu[pos:])
+		dec := pos - lPos
+		p.queu = p.queu[:len(p.queu)-dec]
+	}
+}
+
+func (p pQueue) Get() (point, bool) {
+	if len(p.queu) == 0 {
+		return point{}, false
+	}
+	return p.queu[0], true
+}
+
+func (p *pQueue) Remove() {
+	if len(p.queu) == 0 {
+		return
+	}
+	//p.queu = p.queu[1:]
+	copy(p.queu, p.queu[1:])
+	p.queu = p.queu[:len(p.queu)-1]
+}
+
 func getSkylineVector(buildings [][]int) [][]int {
 	if len(buildings) == 0 {
 		return nil
@@ -39,7 +203,7 @@ func getSkylineVector(buildings [][]int) [][]int {
 
 	//main idea - work with points and insert new points to return vector
 	//cons - its slow because slice copying. dual linked list will be better but ill lose binary search in that case...
-	var ret [][]int
+	var ret []point
 	for _, building := range buildings {
 		l := building[0]
 		r := building[1]
@@ -50,7 +214,7 @@ func getSkylineVector(buildings [][]int) [][]int {
 		//insert turn points as necessary
 
 		lPos := sort.Search(len(ret), func(i int) bool {
-			return ret[i][0] >= l
+			return ret[i].x >= l
 		})
 
 		//do merge insert
@@ -58,59 +222,124 @@ func getSkylineVector(buildings [][]int) [][]int {
 		var prevH int
 		switch {
 		case lPos >= len(ret): // add points to end
-			ret = append(ret, []int{l, h})
-			ret = append(ret, []int{r, 0})
+			ret = append(ret, point{x: l, h: h})
+			ret = append(ret, point{x: r, h: 0})
 			continue
-		case ret[lPos][0] != l: //insert before
+		case ret[lPos].x != l: //insert before
 			switch {
 			case lPos == 0: //no prev value
-				ret = append(ret, []int{0, 0})
+				ret = append(ret, point{x: 0, h: 0})
 				copy(ret[lPos+1:], ret[lPos:])
-				ret[lPos] = []int{l, h}
+				ret[lPos] = point{x: l, h: h}
 				prevH = h
-			case ret[lPos-1][1] < h: //insert new point
-				ret = append(ret, []int{0, 0})
+			case ret[lPos-1].h < h: //insert new point
+				ret = append(ret, point{0, 0})
 				copy(ret[lPos+1:], ret[lPos:])
-				prevH = ret[lPos-1][1]
+				prevH = ret[lPos-1].h
 			default: //do nothing
 				prevH = h
 			}
 		default: //the same point
-			prevH = ret[lPos][1]
+			prevH = ret[lPos].h
 		}
 
-		helperMerge(lPos, r, h, prevH, ret[lPos:])
+		helperMerge(lPos, l, r, h, prevH, ret[lPos:])
 		panic("not implemented")
 
 	}
-	return ret
+	panic("not implemented")
+	return [][]int{}
 }
 
-func helperMerge(lPos, r, h, prevH int, data [][]int) [][]int {
+/*
+3 группы ситуаций:
+1 : первая точка
+1.1 первая точка не совпадает ни с одной из существующих. решаем что делать:
+	предыдущая ниже нашей=>вставляем новую точку
+	предыдущая выше или равна нашей=>ничего не делаем
+1.2 первая совпадает с существующей
+	текущая выше нашей=>ничего не делаем
+	текущая ниже либо равна нашей, а наша равна предыдущей=>удаляем текущую
+	в остальных случаях (текущая ниже нашей, а наша ниже\выше предыдущей)=>поднимаем до текущей
+
+2: точки между границами интервала (такой же алгоритм как в 1.2)
+
+3: последняя точка
+3.1 не попадает ни на одну из существующих
+	отличается от предыдущей и отличается от следующей=>вставляем нашу точку
+	меньше предыдущей=>ничего не делаем
+	больше предыдущей и совпадает со следующей=>заменяем следующую
+3.2 попадает на существующую- мы должны упасть до нуля. ничего не делаем
+*/
+
+func helperMerge(lPos, l, r, h, prevH int, data []point) []point {
+	if len(data) < 2 {
+		panic("invalid data")
+	}
+
+	switch {
+	case data[lPos].x != l && prevH < h: //1.1 кейс
+		data = append(data, point{})
+		copy(data[lPos+1:], data[lPos:])
+		data[lPos].x = l
+		data[lPos].h = h
+		prevH = h
+		lPos++
+	case data[lPos].x == l:
+		prevH = data[lPos].h
+		if prevH < h {
+			data[lPos].h = h
+		}
+		lPos++
+	}
 
 	//iterate till r and insert/remove points. there is no prev point....
+	var lastH int
 	var i int
-	for i = lPos; i < len(data) && data[i][1] <= r; i++ {
-		//rx := ret[i][0]
-		rh := data[i][1]
+	for i = lPos; i < len(data)-1 && data[i].h < r; i++ {
+		lastH = data[i].h
+
+		rh := data[i].h
 		switch {
-		case rh <= h && prevH < h: //remove point
-			copy(data[i+1:], data[i:])
-		case rh <= h && prevH >= h: //replace point
-			data[i][1] = h
-		case rh > h: //up point
+		case rh > h: //do nothing
 			prevH = rh
+		case rh <= h && h == prevH: //remove dub
+			copy(data[i:], data[i+1:])
+			data = data[:len(data)-1]
+		default: //up point
+			data[i].h = h
+			prevH = h
 		}
 	}
 	//last point
 	switch {
-	case i >= len(data):
-		data = append(data, []int{r, 0})
-	case data[i][0] > r && data[i][1] < h: //insert new point
-		data = append(data, []int{0, 0})
+	case i >= len(data)-1: //assume it is zero
+		last := &data[len(data)-1]
+		switch {
+		case last.x == r: //do nothing
+		case last.x > r:
+			//изучаем какая предыдущая
+			//либо предыдущая выше нашей-ничего не делаем
+			//либе предыдущая такая же как наша... о как... а мы же потерями информацию - чья это была предыдущая. то ли мы подняли, то ли...
+			//нам нужна информация по владельцу предыдущей точки - мы или не мы. если не мы, то надо знать какая была высота..
+			//зчит нельзя в цикле идти до последней
+			data = append(data, point{})
+			copy(data[i+1:], data[lPos:])
+			last.x = r
+			last.h = lastH
+		case last.x < r:
+			last.h = h
+			data = append(data, point{x: r, h: 0})
+		}
+
+	case data[i].x == r: //do nothing
+	case data[i].x > r:
+		data = append(data, point{})
 		copy(data[i+1:], data[i:])
-		data[i][0] = r
-		data[i][1] = h
+		data[i].x = r
+		data[i].h = lastH
+	case data[i].x < r:
+		panic("impossible case")
 	}
 	return data
 }
--- ms-other/the-skyline-problem_test.go
+++ ms-other/the-skyline-problem_test.go
@@ -10,34 +10,79 @@ func TestGetSkyline(t *testing.T) {
 		buildings [][]int
 		res       [][]int
 	}{
-		{
+		{ //0
+			buildings: [][]int{{15, 20, 10}, {19, 24, 8}},
+			res:       [][]int{{15, 10}, {20, 8}, {24, 0}},
+		},
+		{ //1
 			buildings: [][]int{{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}},
 			res:       [][]int{{2, 10}, {3, 15}, {7, 12}, {12, 0}, {15, 10}, {20, 8}, {24, 0}},
 		},
-		{
+		{ //2
 			buildings: [][]int{{0, 2, 3}, {2, 5, 3}},
 			res:       [][]int{{0, 3}, {5, 0}},
 		},
-		{
+		{ //3
 			buildings: [][]int{{0, 2147483647, 2147483647}},
 			res:       [][]int{{0, 2147483647}, {2147483647, 0}},
+		},
+		{ //4
+			buildings: [][]int{{1, 2, 1}, {1, 2, 2}, {1, 2, 3}},
+			res:       [][]int{{1, 3}, {2, 0}},
+		},
+		{ //5
+			buildings: [][]int{{0, 3, 3}, {1, 5, 3}, {2, 4, 3}, {3, 7, 3}},
+			res:       [][]int{{0, 3}, {7, 0}},
+		},
+		{ //6
+			buildings: [][]int{
+				{1, 38, 219},
+				{2, 19, 228}, {2, 64, 106},
+				{3, 80, 65}, {3, 84, 8},
+				{4, 12, 8}, {4, 25, 14}, {4, 46, 225}, {4, 67, 187},
+				{5, 36, 118}, {5, 48, 211}, {5, 55, 97},
+				{6, 42, 92}, {6, 56, 188},
+				{7, 37, 42}, {7, 49, 78}, {7, 84, 163},
+				{8, 44, 212},
+				{9, 42, 125}, {9, 85, 200}, {9, 100, 74},
+				{10, 13, 58},
+				{11, 30, 179},
+				{12, 32, 215}, {12, 33, 161}, {12, 61, 198},
+				{13, 38, 48}, {13, 65, 222},
+				{14, 22, 1},
+				{15, 70, 222},
+				{16, 19, 196}, {16, 24, 142}, {16, 25, 176}, {16, 57, 114},
+				{18, 45, 1},
+				{19, 79, 149},
+				{20, 33, 53}, {21, 29, 41}, {23, 77, 43}, {24, 41, 75}, {24, 94, 20}, {27, 63, 2}, {31, 69, 58}, {31, 88, 123}, {31, 88, 146}, {33, 61, 27}, {35, 62, 190}, {35, 81, 116}, {37, 97, 81}, {38, 78, 99}, {39, 51, 125}, {39, 98, 144}, {40, 95, 4}, {45, 89, 229}, {47, 49, 10}, {47, 99, 152}, {48, 67, 69}, {48, 72, 1}, {49, 73, 204}, {49, 77, 117}, {50, 61, 174}, {50, 76, 147}, {52, 64, 4}, {52, 89, 84}, {54, 70, 201}, {57, 76, 47}, {58, 61, 215}, {58, 98, 57}, {61, 95, 190}, {66, 71, 34}, {66, 99, 53}, {67, 74, 9}, {68, 97, 175}, {70, 88, 131}, {74, 77, 155}, {74, 99, 145}, {76, 88, 26}, {82, 87, 40}, {83, 84, 132}, {88, 99, 99}},
+			res: [][]int{{1, 219}, {2, 228}, {19, 225}, {45, 229}, {89, 190}, {95, 175}, {97, 152}, {99, 74}, {100, 0}},
 		},
 	}
 
 	for i, test := range tests {
-		res := getSkylineVector(test.buildings)
+		res := getSkylineTry3(test.buildings)
 		assert.Equalf(t, test.res, res, "case(%d)", i)
 	}
 }
 
 func TestHelperMerge(t *testing.T) {
 	tests := []struct {
-		in, out        [][]int
-		l, r, h, prevH int
+		in, out              []point
+		lPos, l, r, h, prevH int
 	}{
 		{ //0
-			in:    [][]int{{1, 2}, {2, 3}, {3, 1}, {4, 2}, {5, 0}},
-			out:   [][]int{{1, 2}, {2, 3}, {3, 2}, {5, 0}},
+			in:    []point{{1, 1}, {2, 0}},
+			out:   []point{{0, 2}, {2, 0}},
+			lPos:  0,
+			l:     0,
+			r:     2,
+			h:     2,
+			prevH: 0,
+		},
+		{ //1
+			in:    []point{{1, 2}, {2, 3}, {3, 1}, {4, 2}, {5, 0}},
+			out:   []point{{1, 2}, {2, 3}, {3, 2}, {5, 0}},
+			lPos:  0,
 			l:     1,
 			r:     4,
 			h:     2,
@@ -46,7 +91,93 @@ func TestHelperMerge(t *testing.T) {
 	}
 
 	for i, test := range tests {
-		res := helperMerge(test.l, test.r, test.h, test.prevH, test.in)
+		res := helperMerge(test.lPos, test.l, test.r, test.h, test.prevH, test.in)
 		assert.Equalf(t, test.out, res, "case(%d)", i)
+	}
+}
+
+func TestPQueue(t *testing.T) {
+	tests := []struct {
+		remove   bool
+		pnt      point
+		internal []point
+	}{
+		{ //0
+			remove:   true,
+			internal: nil,
+		},
+		{ //1
+			remove:   false,
+			pnt:      point{1, 1},
+			internal: []point{{1, 1}},
+		},
+		{ //2
+			remove:   true,
+			internal: []point{},
+		},
+		{ //3
+			remove:   false,
+			pnt:      point{1, 1},
+			internal: []point{{1, 1}},
+		},
+		{ //4
+			remove:   false,
+			pnt:      point{1, 1},
+			internal: []point{{1, 1}},
+		},
+		{ //5
+			remove:   false,
+			pnt:      point{1, 2},
+			internal: []point{{1, 2}},
+		},
+		{ //6
+			remove:   false,
+			pnt:      point{2, 1},
+			internal: []point{{1, 2}, {2, 1}},
+		},
+		{ //7
+			remove:   false,
+			pnt:      point{3, 2},
+			internal: []point{{3, 2}},
+		},
+		{ //8
+			remove:   false,
+			pnt:      point{5, 3},
+			internal: []point{{5, 3}},
+		},
+		{ //9
+			remove:   false,
+			pnt:      point{3, 3},
+			internal: []point{{5, 3}},
+		},
+		{ //10
+			remove:   false,
+			pnt:      point{3, 2},
+			internal: []point{{5, 3}},
+		},
+		{ //11
+			remove:   false,
+			pnt:      point{10, 2},
+			internal: []point{{5, 3}, {10, 2}},
+		},
+		{ //12
+			remove:   true,
+			internal: []point{{10, 2}},
+		},
+		{ //13
+			remove:   false,
+			pnt:      point{20, 2},
+			internal: []point{{20, 2}},
+		},
+	}
+
+	pq := pQueue{}
+	for i, test := range tests {
+		if test.remove {
+			pq.Remove()
+		} else {
+			pq.Add(test.pnt)
+		}
+		assert.Equalf(t, test.internal, pq.queu, "case(%d)", i)
 	}
 }
